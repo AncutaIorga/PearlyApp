fc1e2a0d2e85b10260521da5416bd79a5c36c4dd
Miikanoff <visualneli4@gmail.com>
2026-01-23 16:59:53 +0100

    v.2.2.3 cambio colores general + añadido apartado imagen en CREAR

--- src/app/post-create/post-create.ts
+++ src/app/post-create/post-create.ts
@@ -8,27 +8,44 @@ import { NavbarComponent } from '../shared/navbar/navbar';
   standalone: true,
   imports: [FormsModule, NavbarComponent],
   templateUrl: './post-create.html',
-  styleUrl: './post-create.css'  // ⚠️ Agregar esta línea
+  styleUrl: './post-create.css'
 })
 export class PostCreateComponent {
   text = '';
+  selectedImage: string | null = null;
 
   constructor(
     private postService: PostService,
     private router: Router
   ) {}
 
+  onImageSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeImage() {
+    this.selectedImage = null;
+  }
+
   submit() {
     if (this.text.trim()) {
       this.postService.addPost({
         id: Date.now(),
         user: 'Healthy User',
-        image: 'https://picsum.photos/400/30' + Math.floor(Math.random() * 10),
+        image: this.selectedImage || 'https://picsum.photos/400/30' + Math.floor(Math.random() * 10),
         text: this.text,
         likes: 0
       });
 
       this.text = '';
+      this.selectedImage = null;
       this.router.navigate(['/feed']);
     }
   }
